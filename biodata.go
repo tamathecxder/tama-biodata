d656c49a277928fa5a6faff17166c82e3de582b8
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-03 22:53:58 +0700

    initial commit

---
+++ biodata.go
@@ -0,0 +1,17 @@
+package main
+
+type Biodata struct {
+	Name, LinkedInURL, GithubURL string
+	Age                          int
+}
+
+func GetBiodata() Biodata {
+	tama := Biodata{
+		Name:        "Yudistira Eka Pratama",
+		Age:         21,
+		LinkedInURL: "https://www.linkedin.com/in/yudistira-eka-pratama",
+		GithubURL:   "https://github.com/tamathecxder",
+	}
+
+	return tama
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module tama-biodata
+
+go 1.24.4
