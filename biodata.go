d38a6db328a7135a53d124e6ccd14fb2b989a041
tamathecxder <yudistiraeka.pratama012@gmail.com>
2025-07-03 23:08:54 +0700

    update

--- biodata.go
+++ biodata.go
@@ -2,12 +2,14 @@ package tamabiodata
 
 type Biodata struct {
 	Name, LinkedInURL, GithubURL string
+	Origin                       string
 	Age                          int
 }
 
 func GetBiodata() Biodata {
 	tama := Biodata{
 		Name:        "Yudistira Eka Pratama",
+		Origin:      "Cianjur",
 		Age:         21,
 		LinkedInURL: "https://www.linkedin.com/in/yudistira-eka-pratama",
 		GithubURL:   "https://github.com/tamathecxder",
